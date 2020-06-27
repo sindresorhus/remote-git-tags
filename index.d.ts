5a2b9dba5fd00f8abc24a9a2ae68195ade94e638
Sindre Sorhus <sindresorhus@gmail.com>
2020-06-27 19:02:30 +0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,11 @@
 declare const remoteGitTags: {
-    /**
-    Get tags from a remote Git repo
+	/**
+	Get tags from a remote Git repo.
 
-    @param url - The first number to add.
-    @returns The tags from a remote Git repo.
-    */
-    (url: string): Promise<Map<string, string>>;
+	@param url - The first number to add.
+	@returns The tags from a remote Git repo.
+	*/
+	(url: string): Promise<Map<string, string>>;
 }
 
 export = remoteGitTags;
